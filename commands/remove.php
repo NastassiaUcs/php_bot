403ee5a43bbb1afaa78ddbe1a82f0f336a872e35
root <root@maytm.ru>
2018-05-16 16:39:07 +0300

    fix

--- commands/remove.php
+++ commands/remove.php
@@ -5,8 +5,8 @@ class remove_command extends BaseCommand {
 
     function process($chatId, $text, $userId, $packet)
     {
-    	if(isset($packet["message"]["reply_to_message"])){
+    	if(isset($packet["message"]["reply_to_message"]))
     		return ["type"=>"deleteMessage", "data"=>$packet["message"]["reply_to_message"]["message_id"]];
-    	}
+    	return "Удаление сообщения происходит через реплей";
     }
 }
